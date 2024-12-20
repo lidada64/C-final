053f5cc33110e109ae6f156549775f1b17213897
lidada <lidada317@outlook.com>
2024-12-20 16:52:15 +0800

    updata提交12.20

--- output/update.c
+++ output/update.c
@@ -55,9 +55,8 @@ beginning:
         printf("\ntaste: ");
         scanf("%s", c.tasteC);
 
-            fprintf(temp/*插入在临时文件里*/, "\n\n>.%-10s\n  price:%-10f\n  quantity:%-10d\n  taste:%-10s\n$\n"/*插入"j"作为命名符"&"作为一种菜品的终止符,>作为起始符"*/,
-               c.name3, c.price3, c.num3, c.tasteC);/*删除了j的标序作用*/
-              
+        fprintf(temp, "\n\n>%-10s\n  price:%-10f\n  quantity:%-10d\n  taste:%-10s\n$",
+                c.name3, c.price3, c.num3, c.tasteC);
     }
 
     // **Step 3: 复制目标位置后的数据**
@@ -70,15 +69,15 @@ beginning:
 
     // **Step 4: 替换原文件**
     if (remove(File) != 0) {
-        perror("cant remove original file");
+        perror("cant delete orignal file");
         return;
     }
 
     if (rename("temp.txt", File) != 0) {
-        perror("cant rename temp file");
+        perror("cant rename file");
         return;
     }
-//删A，B，C后名称
+
     printf("Add successfully!\n");
 }
 
@@ -102,128 +101,27 @@ long move() {
     long position = 0; // 记录指针位置
     FILE *p = fopen(File, "r");
     if (p == NULL) {
-        perror("cant open file");
+        perror("cant open the file");
         return -1;
     }
+
     while ((ch = fgetc(p)) != EOF) {
         if (ch == point) {
             position = ftell(p);
             break;
         }
     }
+
     fclose(p);
     return position;
 }
-void delete(long position) {
-    FILE *original = fopen(File, "r");
-    if (original == NULL) {
-        perror("无法打开原文件");
-        return;
-    }
-
-    FILE *temp = fopen("temp.txt", "w");
-    if (temp == NULL) {
-        perror("无法创建临时文件");
-        fclose(original);
-        return;
-    }
-
-    fseek(original, position, SEEK_SET); // 移动到目标大类位置
-
-    int ch, dish_count = 1, current_dish = 0; // 新的菜品编号和当前菜品编号
-    int a[100], num; // 存储需要删除的菜品编号
-
-    // 输入需要删除的菜品编号
-    printf("How many dishes you want to remove? ");
-    scanf("%d", &num);
-    printf("Which dishes to remove? ");
-    for (int i = 0; i < num; i++) {
-        scanf("%d", &a[i]);
-    }
-
-    // 遍历文件内容
-    while ((ch = fgetc(original)) != EOF && ch != 'E') {
-        if (ch == '>') { // 新菜品的开始
-            current_dish++;
-            int to_delete = 0;
-
-            // 检查当前菜品是否需要删除
-            for (int i = 0; i < num; i++) {
-                if (a[i] == current_dish) {
-                    to_delete = 1;
-                    break;
-                }
-            }
-
-            if (to_delete) {
-                // 跳过当前菜品内容
-                while ((ch = fgetc(original)) != EOF && ch != '$');
-                continue;
-            } else {
-                // 保留菜品并重新编号
-                fprintf(temp, ">.%d", dish_count++); // 重新编号
-                while ((ch = fgetc(original)) != EOF) {
-                    fputc(ch, temp);
-                    if (ch == '$') {
-                        fputc('\n', temp);
-                        break;
-                    }
-                }
-            }
-        } else {
-            // 非菜品部分直接复制
-            fputc(ch, temp);
-        }
-    }
-
-    // 将剩余内容复制到临时文件
-    while ((ch = fgetc(original)) != EOF) {
-        fputc(ch, temp);
-    }
-
-    fclose(original);
-    fclose(temp);
-
-    // 替换原文件
-    if (remove(File) != 0) {
-        perror("无法删除原文件");
-        return;
-    }
-
-    if (rename("temp.txt", File) != 0) {
-        perror("无法重命名临时文件");
-        return;
-    }
-
-    printf("Remove successfully!\n");
-}
 
 int main() {
-    int fuc;
     long position = move(); // 获取目标位置
-    error:
-    printf("Which function you want to use?\n1.append\t2.remove\n>:");
-    scanf("%d",&fuc);
-    switch(fuc){
-        case 1:
-        if (position > 0) {
+    if (position > 0) {
         append_at_position(position); // 在指定位置追加数据
-        } else {
+    } else {
         printf("cant find the position\n");
-        }
-        break;
-        case 2:
-        if (position > 0) {
-        delete(position); // 删除功能
-        } else {
-        printf("cant find the position\n");
-        }
-        break;
-        default:
-        printf("error\n");
-        goto error;//如果错了的话可以跳到原来的位置
-        
     }
-    
     return 0;
 }
--- specific_point.c
+++ specific_point.c
@@ -1,42 +1,102 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>//引入为了防止文件里出现大写
+#define File "C:\\Users\\12281\\Desktop\\stash.txt"
+//这个代码功能，将文件中的数据放在数组中，更新后在输入进文件
 
-int main() {
-    FILE *file;
-    char ch;
-    char target_char = 'X'; // 这里假设你要找的字符是 'X'
-    long position = 0;
-
-    // 打开文件
-    file = fopen("C:\\Users\\12281\\Desktop\\stash.txt", "r");
-    if (file == NULL) {
-        perror("无法打开文件");
-        return 1;
+  typedef struct hot_dish
+    {	
+        int num3;
+       float price3;
+        char name3[20];
+        char tasteC[20];
+      
+    }C; 
+void append(int position){
+    int i;
+   beginning:
+    printf("How many food you would like to add: ");
+    scanf("%d",&i);
+   C c;
+FILE *ap=fopen(File,"r+");//记得要写关闭
+fseek(ap,position,SEEK_SET);
+    if(i<=0){
+        printf("ERROR!!!!");
+        goto beginning;
     }
-
-    // 遍历文件直到找到目标字符
-    while ((ch = fgetc(file)) != EOF) {
-        if (ch == target_char) {
+    else
+    for(int j=0;j<i;j++){
+    printf("---------------------------------\nEnter details of dish %d ",j+1);
+    printf("name: ");
+    scanf("%s",c.name3);
+    printf("---------------------------------\n");
+    printf("\nprice: ");
+    scanf("%f",&c.price3);
+    printf("\nquantitiy: ");
+    scanf("%d",&c.num3);
+    printf("\ntaste: ");
+    scanf("%s",c.tasteC);
+    fprintf(ap,"\n\n%-10s\n  price:%-10f\n  quantitiy:%-10d\n  taste:%-10s\n",c.name3,c.price3,c.num3,c.tasteC);
+    }
+    fclose(ap);
+}//append system
+int move(){
+    printf("Which kind of food you want to add(1-3):\n");
+    int t;
+    char ch;//指针寻找字符
+    char point;
+    do{
+    printf("1.staple\t2.cold_dish\t3.hot_dish\n");
+    printf(" >: ");
+    scanf("%d",&t);
+    }while(t!=1&&t!=2&&t!=3);
+    switch (t)
+    {   
+        case 1:
+        point='A';//指针要找到的字符
+        break;
+        case 2:
+        point='B';
+        break;
+        case 3:
+        point='C';
+        break;
+    }
+    long position=0;//指针位置
+    //将分类前数字标志改为大写字母
+    FILE *p=fopen(File,"r");
+    while ((ch = fgetc(p)) != EOF) {
+        if (ch == point) {
             // 找到了目标字符，保存当前位置
-            position = ftell(file);
+            position = ftell(p);
             break;
         }
-    }
-
-    // 如果未找到目标字符，position 保持为 0
-    if (position != 0) {
+    }//从文件开头遍历文件
+       if (position != 0) {
         // 移动到目标字符的前一个位置
-        if (fseek(file, position - 1, SEEK_SET) != 0) {
+        if (fseek(p, position - 1, SEEK_SET/*文件开头*/) != 0) {
             perror("fseek 失败");
-            fclose(file);
-            return 1;
+            fclose(p);
+            
         }
-        
-        // 在这里你可以执行一些操作，如读取或写入
-        printf("文件指针现在指向 %c\n", fgetc(file)); // 应该打印出目标字符前的字符
-    } else {
-        printf("未找到指定字符\n");
-    }
+        }  
+        while((ch = fgetc(p)) != EOF){
+            if(ch=='\n'||ch=='E'){
+                position=ftell(p);
+                break;
+            }
+            }
+        while ((ch = fgetc(p)) != EOF) {
+            if (ch != '\n' && ch != '\r') { // 跳过空行
+                fseek(p, -1, SEEK_CUR); // 将指针移动到有效字符位置
+                break;
+            }
+        }
+        return position;
+}//pointer movement system
 
-    fclose(file);
-    return 0;
+int main()
+{
+    int position=move();
+    append(position);
+    
 }
